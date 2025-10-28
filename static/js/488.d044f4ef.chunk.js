ad45f8da884c32d33dcc9aee9e527da0e00ba1f4
rdas1402 <r14.das@gmail.com>
2025-10-28 23:41:52 +0530

    Updates

--- static/js/488.d044f4ef.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkvinum_nobile=self.webpackChunkvinum_nobile||[]).push([[488],{488:(e,n,u)=>{new WeakMap;new Set}}]);
-//# sourceMappingURL=488.d044f4ef.chunk.js.map
---
+++ static/js/488.da89d156.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmaximony_app=self.webpackChunkmaximony_app||[]).push([[488],{488:(e,a,p)=>{new WeakMap;new Set}}]);
+//# sourceMappingURL=488.da89d156.chunk.js.map
